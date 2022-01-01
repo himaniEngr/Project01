2f63fafc70a4ca4ca2baf0cd0212c6bc82f0133a
Himani <himani9.engg@gmail.com>
2022-01-01 15:19:50 +0530

    implemented smooth scrolling using javascript part 2

--- js/script.js
+++ js/script.js
@@ -30,13 +30,25 @@ allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     e.preventDefault();
     const href = link.getAttribute("href");
-    console.log(href);
-
+    // console.log(href);
+    //scroll back to top
     if (href == "#")
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
+    //scroll to other links
+    if (href != "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      // console.log(sectionEl);
+      sectionEl.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+    //close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+    // console.log(href);
   });
 });
 
